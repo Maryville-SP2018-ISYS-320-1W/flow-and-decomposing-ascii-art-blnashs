7cb3e3f5527ab7720d0550456e047e3df01c3141
Barbara Nash <bnash1@live.maryville.edu>
2018-03-18 11:26:49 -0500

    Comment String

--- src/Tricky.java
+++ src/Tricky.java
@@ -5,13 +5,27 @@
 */
 
 // 1. Your predicted output 
+// That this will run the first assignment
  
 
-public class Tricky {
+public class Tricky {public class AboutYou {
+
+	public static void main(String[] args) {
+		// TODO Modify the values in the String literals in ALL_CAPS below
+		System.out.print("Hello, my name is ");
+		System.out.println("Barbara Nash");
+
+		System.out.print("And I'm taking this class because ");
+		System.out.println("I love learning new things and this course will teach me about Java");
+
+	}
 
 }
 
+
+}
 // 3. Were you correct? Explain any differences
+//  It ran and gave the results of the first assignment, I expected to see both the original and results
  
  
 
